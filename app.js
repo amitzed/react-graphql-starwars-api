643531e206abd0ce2e6b5a933bf587cc362ec836
amitzed <amitzmn@gmail.com>
2020-10-27 16:30:29 -0400

    Created Schema

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server');
+const axios = require('axios');
+
+const typeDefs = gql`
+  type Person {
+    id: ID
+    name: String
+    birth_year: String
+    height: String
+    mass: String
+    hair_color: String
+    gender: String
+  }
+
+  type Query {
+    people: [Person]
+  }
+`;
